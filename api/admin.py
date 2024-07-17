4c7c2e6311445b7ac41a09f9cdbc7bd416f1d837
Alonso-Valdivia <34718875+Adriana618@users.noreply.github.com>
2024-07-17 13:39:49 -0500

    feat[ci]: Add Actions for test PR (#2)
    * feat[ci]: Add Actions for test PR
    
    * feat[ci]: Add vulnerability scan
    
    * test[ci]: Dismiss threshold for test all pipeline
    
    * feat[requirements]: Add pip-tools for dependencies management
    
    * fix[pytest]: Add appropiate configuration
    
    * test[ci]: Check trivy action
    
    * test[ci]: Check trivy action
    
    * test[ci]: Check trivy action
    
    * fix[ci]: Correct use of envars in actions
    
    * fix[ci]: Correct use of envars in actions
    
    * fix[ci]: Correct use of envars in actions
    
    * test[ci]: Deactivate vulnerability scan for test all pipeline
    
    * feat[lint]: Add pylint configuration

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,10 @@
+"""
+Module for registering models with the Django admin site.
+
+This module registers the User model with the Django admin site, allowing it to be managed
+through the admin interface.
+"""
+
 from django.contrib import admin
 from .models import User
 
